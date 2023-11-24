96cd2e8c3fcb563459f6013703d6460cc36cb7f8
jalalfaisal <59533484+jalalfaisal@users.noreply.github.com>
2023-11-24 09:35:23 +0000

    A simple python program that calculate the Square root of a number.

---
+++ sqrt_calculator.py
@@ -0,0 +1,10 @@
+import math
+
+# Accept input from user
+num = float(input("Enter a number: "))
+
+# Calculate the square root
+sqrt = math.sqrt(num)
+
+# Display the result
+print("The square root of", num, "is", sqrt)
