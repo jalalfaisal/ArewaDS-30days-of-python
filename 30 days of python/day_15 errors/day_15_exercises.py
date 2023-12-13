902b599b9b0061d3d4e5a70224a5c399996dde9f
jalalfaisal <59533484+jalalfaisal@users.noreply.github.com>
2023-12-13 07:14:45 +0000

    day_15 commit

---
+++ 30 days of python/day_15 errors/day_15_exercises.py
@@ -0,0 +1,80 @@
+#SyntaxError
+#Example 1: SyntaxError
+"""
+asabeneh@Asabeneh:~$ python
+Python 3.9.6 (default, Jun 28 2021, 15:26:21)
+[Clang 11.0.0 (clang-1100.0.33.8)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+print 'hello world'
+  File "<stdin>", line 1
+    print 'hello world'
+
+#Example 1: NameError
+
+asabeneh@Asabeneh:~$ python
+Python 3.9.6 (default, Jun 28 2021, 15:26:21)
+[Clang 11.0.0 (clang-1100.0.33.8)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+print(age)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+
+#Example 1: IndexError
+
+asabeneh@Asabeneh:~$ python
+Python 3.9.6 (default, Jun 28 2021, 15:26:21)
+[Clang 11.0.0 (clang-1100.0.33.8)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+numbers = [1, 2, 3, 4, 5]
+numbers[5]
+
+#Example 1: ModuleNotFoundError
+
+asabeneh@Asabeneh:~$ python
+Python 3.9.6 (default, Jun 28 2021, 15:26:21)
+[Clang 11.0.0 (clang-1100.0.33.8)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+ import maths
+#attribute 
+import math
+ math.PI
+#Example 1: KeyError
+
+asabeneh@Asabeneh:~$ python
+Python 3.9.6 (default, Jun 28 2021, 15:26:21)
+[Clang 11.0.0 (clang-1100.0.33.8)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+ users = {'name':'Asab', 'age':250, 'country':'Finland'}
+ users['name']
+'Asab'
+users['county']
+
+#Example 1: TypeError
+
+asabeneh@Asabeneh:~$ python
+Python 3.9.6 (default, Jun 28 2021, 15:26:21)
+[Clang 11.0.0 (clang-1100.0.33.8)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+ 4 + '3'
+#ImportError
+#Example 1: TypeError
+
+asabeneh@Asabeneh:~$ python
+Python 3.9.6 (default, Jun 28 2021, 15:26:21)
+[Clang 11.0.0 (clang-1100.0.33.8)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+
+#ValueError
+asabeneh@Asabeneh:~$ python
+Python 3.9.6 (default, Jun 28 2021, 15:26:21)
+[Clang 11.0.0 (clang-1100.0.33.8)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+ int('12a')
+
+ #ZeroDivisionError
+asabeneh@Asabeneh:~$ python
+Python 3.9.6 (default, Jun 28 2021, 15:26:21)
+[Clang 11.0.0 (clang-1100.0.33.8)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+ 1/0
+ """
